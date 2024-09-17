f58003c8ba1e4c63c49e221a4c7f5f6cd6f67dd7
SazarCode <66286300+CerberusProgrammer@users.noreply.github.com>
2024-09-17 16:18:34 -0700

    Initial commit

---
+++ src/data/index.ts
@@ -0,0 +1,27 @@
+export const SITE_TITLE = "Sanju's Space";
+export const SITE_DESCRIPTION = "A internet space for Sanju.";
+
+export interface MenuItem {
+	label: string;
+	url: string;
+}
+
+// Menu items
+export const menuItems: MenuItem[] = [
+	{
+		label: "home",
+		url: "/",
+	},
+	{
+		label: "writings",
+		url: "/writings",
+	},
+	{
+		label: "thoughts",
+		url: "/thoughts",
+	},
+	{
+		label: "ships",
+		url: "/ships",
+	},
+];
