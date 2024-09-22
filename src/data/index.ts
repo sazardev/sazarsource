c7c4df260b7d59bcd122d5d1277327718f73a61b
Omar Flores <cerberusprogrammer@gmail.com>
2024-09-22 08:04:23 -0700

    Refactor site title and description

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,27 +1,27 @@
-export const SITE_TITLE = "Sanju's Space";
-export const SITE_DESCRIPTION = "A internet space for Sanju.";
+export const SITE_TITLE = "Sazar's Space";
+export const SITE_DESCRIPTION = "A internet space for Sazar.";
 
 export interface MenuItem {
-	label: string;
-	url: string;
+  label: string;
+  url: string;
 }
 
 // Menu items
 export const menuItems: MenuItem[] = [
-	{
-		label: "home",
-		url: "/",
-	},
-	{
-		label: "writings",
-		url: "/writings",
-	},
-	{
-		label: "thoughts",
-		url: "/thoughts",
-	},
-	{
-		label: "ships",
-		url: "/ships",
-	},
+  {
+    label: "home",
+    url: "/",
+  },
+  {
+    label: "writings",
+    url: "/writings",
+  },
+  {
+    label: "thoughts",
+    url: "/thoughts",
+  },
+  {
+    label: "ships",
+    url: "/ships",
+  },
 ];
