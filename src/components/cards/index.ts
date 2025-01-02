997f38bddb3e200516570a8c618b2967f60b42b4
Omar Flores <omar.desarrollo@gmtransporterp.com>
2025-01-02 15:07:14 -0800

    Add CoursesItem component to cards index for improved course display

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -3,3 +3,4 @@ export { default as WritingItem } from "./writing-item.astro";
 export { default as ShipsItem } from "./ships-item.astro";
 export { default as TalesItem } from "./tale-item.astro";
 export { default as BooksItem } from "./books-item.astro";
+export { default as CoursesItem } from "./courses-item.astro";
