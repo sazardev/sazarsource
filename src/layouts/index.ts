88b372849d79b1775e2ae18db3c403394d407ca5
Omar Flores <omar.desarrollo@gmtransporterp.com>
2025-01-02 14:44:54 -0800

    Add TalesLayout export to layouts for new tales section

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -2,3 +2,4 @@ export { default as WritingLayout } from "./writing-layout.astro";
 export { default as ShipsLayout } from "./ships-layout.astro";
 export { default as ThoughtsLayout } from "./thoughts-layout.astro";
 export { default as RootLayout } from "./root-layout.astro";
+export { default as TalesLayout } from "./tales-layout.astro";
