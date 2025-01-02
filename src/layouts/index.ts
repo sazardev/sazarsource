8e668071f560356fbce46b6a410886121e24a489
Omar Flores <omar.desarrollo@gmtransporterp.com>
2025-01-02 14:49:04 -0800

    Add BooksLayout export to layouts for new books section

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -3,3 +3,4 @@ export { default as ShipsLayout } from "./ships-layout.astro";
 export { default as ThoughtsLayout } from "./thoughts-layout.astro";
 export { default as RootLayout } from "./root-layout.astro";
 export { default as TalesLayout } from "./tales-layout.astro";
+export { default as BooksLayout } from "./books-layout.astro";
