1601b7b8fab148a6984574de7197fcae5a781e44
Omar Flores <omar.desarrollo@gmtransporterp.com>
2025-01-02 14:53:52 -0800

    Add BooksItem export to cards component for book details rendering

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -2,3 +2,4 @@ export { default as ThoughtsItem } from "./thoughts-item.astro";
 export { default as WritingItem } from "./writing-item.astro";
 export { default as ShipsItem } from "./ships-item.astro";
 export { default as TalesItem } from "./tale-item.astro";
+export { default as BooksItem } from "./books-item.astro";
